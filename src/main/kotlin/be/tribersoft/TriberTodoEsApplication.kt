5a5873ef8cd67bdbdbd1654970ec03e47a63a2ae
geert olaerts <geertolaerts@geerts-MBP.lan>
2016-10-10 18:11:08 +0200

    now the kotlin way

--- src/main/kotlin/be/tribersoft/TriberTodoEsApplication.kt
+++ src/main/kotlin/be/tribersoft/TriberTodoEsApplication.kt
@@ -1,11 +1,33 @@
 package be.tribersoft
 
+import be.tribersoft.api.CreateTodoItemCommand
+import be.tribersoft.api.CreateTodoListCommand
+import be.tribersoft.api.TodoListCreatedEvent
+import org.axonframework.commandhandling.CommandBus
+import org.axonframework.commandhandling.GenericCommandMessage
+import org.axonframework.eventsourcing.eventstore.EventStorageEngine
+import org.axonframework.eventsourcing.eventstore.inmemory.InMemoryEventStorageEngine
+import org.axonframework.spring.config.EnableAxon
+import org.hibernate.event.spi.EventSource
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
 
+
+@EnableAxon
 @SpringBootApplication
-open class TriberTodoEsApplication
+open class TriberTodoEsApplicationKotlin {
+
+    @Bean
+    open fun eventStorageEngine(): EventStorageEngine {
+        return InMemoryEventStorageEngine()
+    }
+
+}
 
 fun main(args: Array<String>) {
-    SpringApplication.run(TriberTodoEsApplication::class.java, *args)
+    val context = SpringApplication.run(TriberTodoEsApplicationKotlin::class.java, *args)
+    val commandBus = context.getBean(CommandBus::class.java)
+    commandBus.dispatch(GenericCommandMessage.asCommandMessage<Any>(CreateTodoListCommand("uuid", "name")))
 }
+
