79cfe556f4ec5f297be78b0bace0958b1ca1c97d
geert olaerts <geertolaerts@geerts-MBP.lan>
2016-10-10 21:20:44 +0200

    switched to futures

--- src/main/kotlin/be/tribersoft/command/TodoListController.kt
+++ src/main/kotlin/be/tribersoft/command/TodoListController.kt
@@ -5,9 +5,10 @@ import be.tribersoft.api.UpdateTodoListNameCommand
 import org.axonframework.commandhandling.CommandBus
 import org.axonframework.commandhandling.GenericCommandMessage
 import org.axonframework.commandhandling.gateway.CommandGateway
-import org.axonframework.commandhandling.model.AggregateNotFoundException
+import org.springframework.scheduling.annotation.Async
 import org.springframework.web.bind.annotation.*
 import java.util.*
+import java.util.concurrent.CompletableFuture
 import javax.inject.Inject
 import javax.ws.rs.core.MediaType
 
@@ -18,19 +19,17 @@ class TodoListController
 constructor(private val commandBus: CommandBus, private val commandGateway: CommandGateway) {
 
     @PostMapping(consumes = arrayOf(MediaType.APPLICATION_JSON), produces = arrayOf(MediaType.APPLICATION_JSON))
-    fun create(@RequestBody body: CreateTodoListJson): TodoListJson {
+    fun create(@RequestBody body: CreateTodoListJson): CompletableFuture<TodoListJson> {
         val uuid = UUID.randomUUID()
-        commandGateway.send<Any>(GenericCommandMessage.asCommandMessage<Any>(CreateTodoListCommand(uuid, body.name)))
-        return TodoListJson(uuid, body.name)
+        val future = commandGateway.send<Any>(GenericCommandMessage.asCommandMessage<Any>(CreateTodoListCommand(uuid, body.name)))
+        return future.thenApply { TodoListJson(uuid, body.name) }
     }
 
     @PutMapping(path = arrayOf("/{uuid}"), consumes = arrayOf(MediaType.APPLICATION_JSON), produces = arrayOf(MediaType.APPLICATION_JSON))
-    fun update(@PathVariable uuid: UUID, @RequestBody body: UpdateTodoListJson): TodoListJson {
-        try {
-            commandGateway.send<Any>(GenericCommandMessage.asCommandMessage<Any>(UpdateTodoListNameCommand(uuid, body.name)))
-        } catch(e: AggregateNotFoundException) {
-            println("----------- not found")
-        }
-        return TodoListJson(uuid, body.name)
+    @Async
+    fun update(@PathVariable uuid: UUID, @RequestBody body: UpdateTodoListJson): CompletableFuture<TodoListJson> {
+        val future = commandGateway.send<Any>(GenericCommandMessage.asCommandMessage<Any>(UpdateTodoListNameCommand(uuid, body.name)))
+        return future.thenApply { TodoListJson(uuid, body.name) }
+
     }
 }
