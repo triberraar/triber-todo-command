59d9e346baaaa92b9a8ad29743ac48247d01c2fd
geert olaerts <geertolaerts@geerts-MBP.lan>
2016-10-10 20:38:41 +0200

    use commandbus

--- src/main/kotlin/be/tribersoft/command/TodoListController.kt
+++ src/main/kotlin/be/tribersoft/command/TodoListController.kt
@@ -4,6 +4,7 @@ import be.tribersoft.api.CreateTodoListCommand
 import be.tribersoft.api.UpdateTodoListNameCommand
 import org.axonframework.commandhandling.CommandBus
 import org.axonframework.commandhandling.GenericCommandMessage
+import org.axonframework.commandhandling.gateway.CommandGateway
 import org.axonframework.commandhandling.model.AggregateNotFoundException
 import org.springframework.web.bind.annotation.*
 import java.util.*
@@ -14,19 +15,19 @@ import javax.ws.rs.core.MediaType
 @RequestMapping(path = arrayOf("/todo-list"))
 class TodoListController
 @Inject
-constructor(private val commandBus: CommandBus) {
+constructor(private val commandBus: CommandBus, private val commandGateway: CommandGateway) {
 
     @PostMapping(consumes = arrayOf(MediaType.APPLICATION_JSON), produces = arrayOf(MediaType.APPLICATION_JSON))
     fun create(@RequestBody body: CreateTodoListJson): TodoListJson {
         val uuid = UUID.randomUUID()
-        commandBus.dispatch(GenericCommandMessage.asCommandMessage<Any>(CreateTodoListCommand(uuid, body.name)))
+        commandGateway.send<Any>(GenericCommandMessage.asCommandMessage<Any>(CreateTodoListCommand(uuid, body.name)))
         return TodoListJson(uuid, body.name)
     }
 
     @PutMapping(path = arrayOf("/{uuid}"), consumes = arrayOf(MediaType.APPLICATION_JSON), produces = arrayOf(MediaType.APPLICATION_JSON))
     fun update(@PathVariable uuid: UUID, @RequestBody body: UpdateTodoListJson): TodoListJson {
         try {
-            commandBus.dispatch(GenericCommandMessage.asCommandMessage<Any>(UpdateTodoListNameCommand(uuid, body.name)))
+            commandGateway.send<Any>(GenericCommandMessage.asCommandMessage<Any>(UpdateTodoListNameCommand(uuid, body.name)))
         } catch(e: AggregateNotFoundException) {
             println("----------- not found")
         }
--- src/main/kotlin/be/tribersoft/config/config.kt
+++ src/main/kotlin/be/tribersoft/config/config.kt
@@ -1,19 +1,27 @@
 package be.tribersoft.config
 
+import org.axonframework.commandhandling.CommandBus
+import org.axonframework.commandhandling.gateway.CommandGateway
+import org.axonframework.commandhandling.gateway.DefaultCommandGateway
 import org.axonframework.eventsourcing.eventstore.EventStorageEngine
 import org.axonframework.eventsourcing.eventstore.inmemory.InMemoryEventStorageEngine
-import org.axonframework.spring.config.EnableAxon
-import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import javax.inject.Inject
 
 @Configuration
-open class AxonConfig {
+open class AxonConfig
+@Inject
+constructor(private val commandBus: CommandBus) {
 
     @Bean
     open fun eventStorageEngine(): EventStorageEngine {
         return InMemoryEventStorageEngine()
     }
 
+    @Bean
+    open fun commandGateway(): CommandGateway {
+        return DefaultCommandGateway(commandBus)
+    }
 
 }
