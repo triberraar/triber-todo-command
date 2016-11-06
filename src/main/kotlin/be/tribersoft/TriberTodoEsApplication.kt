fedce32541233969644423e193fedac626b441e7
geert olaerts <geertolaerts@geerts-MBP.lan>
2016-11-06 20:17:00 +0100

    added discoveryclient

--- src/main/kotlin/be/tribersoft/TriberTodoEsApplication.kt
+++ src/main/kotlin/be/tribersoft/TriberTodoEsApplication.kt
@@ -3,10 +3,12 @@ package be.tribersoft
 import org.axonframework.spring.config.EnableAxon
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 
 @EnableAxon
 @SpringBootApplication
+@EnableDiscoveryClient
 open class TriberTodoEsApplicationKotlin {
 
 }
